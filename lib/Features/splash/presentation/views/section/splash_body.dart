964daa86e43d69fadc5d802570149bc76999e4ce
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-01-23 19:02:06 +0200

    Add sliding animation and navigate to home view on splash screen

--- lib/Features/splash/presentation/views/section/splash_body.dart
+++ lib/Features/splash/presentation/views/section/splash_body.dart
@@ -1,8 +1,55 @@
+import 'package:bookly/Features/home/presentation/views/home_view.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashBody extends StatelessWidget {
+import '../../../../../constants.dart';
+import '../widgets/sliding_text.dart';
+
+class SplashBody extends StatefulWidget {
   const SplashBody({super.key});
+
+  @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    intiSlideAnimation();
+
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => HomeView(),
+          transition: Transition.fade, duration: kTransitionDuration);
+    });
+  }
+
+  void intiSlideAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +58,12 @@ class SplashBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 10,
+        ),
+        SlidingText(
+            animationController: animationController,
+            slidingAnimation: slidingAnimation)
       ],
     );
   }
---
+++ lib/Features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.animationController,
+    required this.slidingAnimation,
+  });
+
+  final AnimationController animationController;
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: animationController,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              'Read Your Favorite Books',
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
+  }
+}
